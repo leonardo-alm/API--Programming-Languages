86aa43088e185f242a4c58952a4fb9c8708ac2c2
Leonardo Almeida <leonardo.olv94@gmail.com>
2022-08-24 14:58:34 -0300

    adding post method

--- ProgrammingLanguages/Controllers/LanguageController.cs
+++ ProgrammingLanguages/Controllers/LanguageController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using ProgrammingLanguages.Dtos;
 using ProgrammingLanguages.Models;
 using System.Text.Json;
+using System.IO;
 
 namespace ProgrammingLanguages.Controllers
 {
@@ -14,22 +16,65 @@ namespace ProgrammingLanguages.Controllers
         public LanguageController(ILogger<LanguageController> logger)
         {
             _logger = logger;
-            using var reader = new StreamReader("./languages.json");
-            string json = reader.ReadToEnd();
-            languages = JsonSerializer.Deserialize<List<Language>>(json);
+
         }
 
         [HttpGet]
-        public IEnumerable<Language> Get()
+        public ActionResult<IEnumerable<Language>> Get()
         {
+            using var reader = new StreamReader("./languages.json");
+            string json = reader.ReadToEnd();
+            languages = JsonSerializer.Deserialize<List<Language>>(json);
             return languages;
         }
 
         [HttpGet("byyear")]
-        public IEnumerable<Language> GetLanguageByYear([FromQuery]string year )
+        public ActionResult<IEnumerable<Language>> GetLanguageByYear([FromQuery]string year, [FromQuery] int page = 1, [FromQuery] int pageSize = 5)
         {
-            var languagesByYear = languages.Where(y => y.Year == year).ToList();
-            return languagesByYear;
+            using var reader = new StreamReader("./languages.json");
+            string json = reader.ReadToEnd();
+            languages = JsonSerializer.Deserialize<List<Language>>(json);
+
+            if (!string.IsNullOrWhiteSpace(year))
+            {
+                var languagesByYear = languages
+                    .Where(y => y.Year == year)
+                    .Skip((page - 1) * pageSize)
+                    .Take(pageSize)
+                    .ToList();
+                
+                if (languagesByYear == null)
+                {
+                    return NotFound();
+                }
+                return languagesByYear;
+            }
+            
+            else
+            {
+                return languages;
+            }
+        }
+        [HttpPost]
+        public object CreateLanguage([FromBody] NewLanguage request)
+        {
+            var reader = new StreamReader("./languages.json");
+            string json = reader.ReadToEnd();
+            reader.Dispose();
+            languages = JsonSerializer.Deserialize<List<Language>>(json);
+
+            var language = new Language
+            {
+                Year = request.Year,
+                Name = request.Name,
+                Chiefdevelopercompany = request.Chiefdevelopercompany,
+                Predecessors = request.Predecessors
+            };
+            languages.Add(language);
+            var content = JsonSerializer.Serialize(languages);
+            System.IO.File.WriteAllText("./languages.json", content);
+            return null;
+
         }
     }
 }
---
+++ ProgrammingLanguages/Dtos/NewLanguage.cs
@@ -0,0 +1,12 @@
+﻿namespace ProgrammingLanguages.Dtos
+{
+    public class NewLanguage
+    {
+        public string Year { get; set; }
+        public string Name { get; set; }
+        public string Chiefdevelopercompany { get; set; }
+        public string Predecessors { get; set; }
+    }
+}
+
+
