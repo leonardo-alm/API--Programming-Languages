eac8782040a9ae569eb74edc1f01a8a9ce778d79
Leonardo Almeida <leonardo.olv94@gmail.com>
2022-08-26 13:33:24 -0300

    adding apiconsumer

---
+++ ApiConsumer/Language.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ApiConsumer
+{
+    public class Language
+    {
+        public string Year { get; set; }
+        public string Name { get; set; }
+        public string Chiefdevelopercompany { get; set; }
+        public string Predecessors { get; set; }
+    }
+}
+
---
+++ ApiConsumer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace ApiConsumer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string url = "https://localhost:44338/languages";
+
+            GetAllLanguages(url).GetAwaiter().GetResult();
+        }
+        public static async Task GetAllLanguages(string url)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
+            List<Language> languages = JsonSerializer.Deserialize<List<Language>>(content);
+
+            foreach (var language in languages)
+            {
+                Console.WriteLine($"{language.Name} was created in {language.Year} by {language.Chiefdevelopercompany} and inspired by {language.Predecessors}");
+            }
+        }
+
+    }
+
+}
+
+
+
